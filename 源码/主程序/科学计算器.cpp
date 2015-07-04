055cca113f67f2b3e9c25cb9b91d0b493a2d9dec
TZY233 <1728936494@qq.com>
2015-07-04 19:11:35 +0800

    新增科学计算器
    删除一些文件

---
+++ 源码/主程序/科学计算器.cpp
@@ -0,0 +1,275 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <windows.h> 
+#include<math.h>
+#include <iostream>
+#include <time.h> 
+#include<dos.h>
+#include<conio.h>
+#include<cmath>
+using namespace std;
+int main()
+{
+	system("title 科学计算器 制作：TZY");
+	system("color 87");
+	void HideCursor();
+	{
+		CONSOLE_CURSOR_INFO cursor_info = { 1, 0 };
+		SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor_info);
+	}
+	Q1:
+	printf("1.计算一个数的开方\n");
+	printf("2.计算一个数的平方\n");
+	printf("3.计算一个数的三次方\n");
+	printf("4.计算一个数的开立方\n");
+	printf("5.待定系数法求函数\n");
+	printf("6.计算一个数的N次方\n");
+	printf("7.开一个数的N次方\n");
+	printf("请输入你的选项并摁回车确认：");
+	short num;
+	cin >> num;
+	switch (num)
+	{
+	case 1:
+	{
+			  C1:
+			  int num1;
+			  printf("请输入你要开方的数：");
+			  cin >> num1;
+			  printf("结果是：");
+			  cout << sqrt(num1) << endl;
+			  printf("输入1接着计算开方，输入2返回上级菜单，输入3退出程序。\n");
+			  printf("请输入你的选项并摁回车确认：");
+			  int a;
+			  cin >> a;
+			  if (a == 1)
+			  {
+				  system("cls");
+				  goto C1;
+			  }
+			  else if (a == 2)
+			  {
+				  system("cls");
+				  goto Q1;
+			  }
+			  else if (a == 3)
+			  {
+				  system("exit");
+			  }
+			  break;
+	case 2:
+	{
+		  C2:
+			  long double num3;
+			  long double num4;
+			  printf("请输入你要平方的数：\n");
+			  cin >> num3;
+			  num4 = num3*num3;
+			  printf("结果为%.10f",num4);
+			  printf("输入1接着计算平方，输入2返回上级菜单，输入3退出程序。\n");
+			  printf("请输入你的选项并摁回车确认：");
+			  int a;
+			  cin >> a;
+			  if (a == 1)
+			  {
+				  system("cls");
+				  goto C2;
+			  }
+			  else if (a == 2)
+			  {
+				  system("cls");
+				  goto Q1;
+			  }
+			  else if (a == 3)
+			  {
+				  system("exit");
+			  }
+			  break;
+	}
+	case 3:
+	{
+			  C3:
+			  long double num5, num6;
+			  printf("请输入你要立方的数：");
+			  cin >> num5;
+			  num6 = num5*num5*num5;
+			  printf("计算结果是：%.10f",num6);
+			  printf("输入1接着计算立方，输入2返回上级菜单，输入3退出程序。\n");
+			  printf("请输入你的选项并摁回车确认：");
+			  int a;
+			  cin >> a;
+			  if (a == 1)
+			  {
+				  system("cls");
+				  goto C3;
+			  }
+			  else if (a == 2)
+			  {
+				  system("cls");
+				  goto Q1;
+			  }
+			  else if (a == 3)
+			  {
+				  system("exit");
+			  }
+			  break;
+	}
+	case 4:
+	{
+		  C4:
+			  long double num7;
+			  printf("请输入你要开立方的数：");
+			  cin >> num7;
+			  cout << pow(num7, 1.0 / 3);
+			  printf("\n");
+			  printf("输入1接着计算平方，输入2返回上级菜单，输入3退出程序。\n");
+			  printf("请输入你的选项并摁回车确认：");
+			  int a;
+			  cin >> a;
+			  if (a == 1)
+			  {
+				  system("cls");
+				  goto C4;
+			  }
+			  else if (a == 2)
+			  {
+				  system("cls");
+				  goto Q1;
+			  }
+			  else if (a == 3)
+			  {
+				  system("exit");
+			  }
+			  break;
+     }
+	case 5:
+	{
+			  C5:
+			  int x1, x2, y1, y2;
+			  printf("请输入第一个坐标的X值：");
+			  cin >> x1;
+			  printf("\n");
+			  printf("请输入第一个坐标的Y值：");
+			  cin >> y1;
+			  printf("\n");
+			  printf("请输入第二个坐标的X值：");
+			  cin >> x2;
+			  printf("\n");
+			  printf("请输入第二个坐标的Y值：");
+			  cin >> y2;
+			  printf("\n");
+			  printf("请稍后...\n");
+			  printf("正在计算...\n");
+			  int x3, y3;
+			  x3 = x2 - x1;
+			  y3 = y2 - y1;
+			  long double k;
+			  k = x3/y3;
+			  long double z;
+			  z = k*x1;
+			  long double b;
+			  b = y1 - z;
+			  printf("K值为：%.2f",k);
+			  printf("    ");
+			  printf("B值为：%.2f",b);
+			  printf("    ");
+			  printf("函数解析式为：y=%.2f",k);
+			  printf("x+%.2f",b);
+			  printf("\n");
+			  printf("输入1接着用待定系数法求函数，输入2返回上级菜单，输入3退出程序。\n");
+			  printf("请输入你的选项并摁回车确认：");
+			  int a;
+			  cin >> a;
+			  if (a == 1)
+			  {
+				  system("cls");
+				  goto C5;
+			  }
+			  else if (a == 2)
+			  {
+				  system("cls");
+				  goto Q1;
+			  }
+			  else if (a == 3)
+			  {
+				  system("exit");
+			  }
+			  break;
+    }
+	case 6:
+	{
+			  C6:
+			  long double num8;
+			  long double num9;
+			  long double l;
+			  printf("请输入你要N次方的数：");
+			  cin >> num8;
+			  printf("请输入你要的N为：");
+			  cin >> num9;
+			  l=pow(num8, num9);
+			  cout << l << endl;
+			  printf("输入1接着计算一个数的N次方，输入2返回上级菜单，输入3退出程序。\n");
+			  printf("请输入你的选项并摁回车确认：");
+			  int a;
+			  cin >> a;
+			  if (a == 1)
+			  {
+				  system("cls");
+				  goto C6;
+			  }
+			  else if (a == 2)
+			  {
+				  system("cls");
+				  goto Q1;
+			  }
+			  else if (a == 3)
+			  {
+				  system("exit");
+			  }
+			  break;
+    }
+	case 7:
+	{
+			  printf("请输入你要开的数：");
+			  long double num10, num11,num12;
+			  cin >> num10;
+			  printf("请输入你要开的N为：");
+			  cin >> num11;
+			  num12=pow(num10, 1.0 / num11);
+			  cout << num12 << endl;
+			  printf("输入1接着计算开一个数的N次方，输入2返回上级菜单，输入3退出程序。\n");
+			  printf("请输入你的选项并摁回车确认：");
+			  int a;
+			  cin >> a;
+			  if (a == 1)
+			  {
+				  system("cls");
+				  goto C6;
+			  }
+			  else if (a == 2)
+			  {
+				  system("cls");
+				  goto Q1;
+			  }
+			  else if (a == 3)
+			  {
+				  system("exit");
+			  }
+			  break;
+    }
+	default:
+	{
+			   printf("请输入有效的选项！！\n");
+			   printf("即将返回桌面...\n");
+			   printf("3\n");
+			   Sleep(1000);
+			   printf("2\n");
+			   Sleep(1000);
+			   printf("1\n");
+			   Sleep(1000);
+			   goto Q1;
+	}
+    }
+	}
+	return 0;
+}
