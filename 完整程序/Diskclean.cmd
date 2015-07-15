0f7abd8e4c6c3c0d472e9fd88468982facd9f0ea
TZY233 <1728936494@qq.com>
2015-07-15 22:37:34 +0800

    磁盘整理修复
    RT

--- 完整程序/Diskclean.cmd
+++ 完整程序/Diskclean.cmd
@@ -1,31 +1,79 @@
 @echo off
 
-:0
+title 碎片整理程序
+
+color 87
 
 set num=
 
-set /p num=请输入你要整理的磁盘盘符,比如C,不需要带冒号,要全盘整理请输入数字0:
+set /p num=请输入你要整理的磁盘盘符,比如C,不需要带冒号,要全盘整理请输入数字1:
 
 if "%num%"=="" goto 0
 
-set num=%num:~0,1%
+if "%num%"=="1" goto 1
 
-if "%num%"=="0" goto 1
+0:
 
 @echo.命令正在运行,请耐心等待.
 
-defrag %num%: -f
+defrag %num%: /u /v
 
-@start Chdiskclean.exe
+@each 整理已完成！
 
-@shutdown -s -t 10
+@pause
+
+@exit
 
 :1
 
 @echo.命令正在运行,请耐心等待.
 
-for %%1 in (c: d: e: f: G: h: i: j: k: l: M: N: O: P: Q: R: S: T: U: V: W: X: Y: Z:) do (if exist %%1 defrag %%1 -f)
+defrag c: /u /v
 
-@start Chdiskclean.exe
+defrag d: /u /v
 
-@shutdown -s -t 10
+defrag e: /u /v
+
+defrag f: /u /v
+
+defrag g: /u /v
+
+defrag h: /u /v
+
+defrag i: /u /v
+
+defrag j: /u /v
+
+defrag k: /u /v
+
+defrag l: /u /v
+
+defrag n: /u /v
+
+defrag m: /u /v
+
+defrag o: /u /v
+
+defrag p: /u /v
+
+defrag q: /u /v
+
+defrag r: /u /v
+
+defrag s: /u /v
+
+defrag t: /u /v
+
+defrag u: /u /v
+
+defrag v: /u /v
+
+defrag w: /u /v
+
+defrag x: /u /v
+
+defrag y: /u /v
+
+defrag z: /u /v
+
+@pause
